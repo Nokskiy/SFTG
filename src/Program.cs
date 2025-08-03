a815c792d939114447888d59109053cb8e19e358
Nokskiy <tsfkafsu@yandex.ru>
2025-08-03 21:08:09 +0500

    Standart project architecture

---
+++ src/Program.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.Logging;
+using NeoSimpleLogger;
+
+namespace SFTG;
+
+internal class Program
+{
+    public static readonly Logger ConsoleLogger = new Logger(Logger.OutputType.Console);
+
+    private static void Main() => ConsoleLogger.LogInformation("Program was been launched");
+}
